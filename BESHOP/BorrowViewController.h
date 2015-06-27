717a4823d89188111f9982ffee6e221c1719b5f6
432daiki <shimizudaiki@MacBook-Pro-2.local>
2015-06-27 14:35:39 +0900

    イニシャルコミット

---
+++ BESHOP/BorrowViewController.h
@@ -0,0 +1,15 @@
+//
+//  BorrowViewController.h
+//  BESHOP
+//
+//  Created by 清水 大輝 on 2015/06/27.
+//  Copyright (c) 2015年 Daiki Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BorrowViewController : UIViewController
+
+- (void)returnToMainView;
+
+@end
---
+++ BESHOP/LendViewController.m
@@ -0,0 +1,34 @@
+//
+//  LendViewController.m
+//  BESHOP
+//
+//  Created by 清水 大輝 on 2015/06/27.
+//  Copyright (c) 2015年 Daiki Shimizu. All rights reserved.
+//
+
+#import "LendViewController.h"
+
+@interface LendViewController ()
+
+@end
+
+@implementation LendViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    if([[segue identifier] isEqualToString:@"segue"]) {
+        
+        
+    }
+}
+
+@end
