661873e84410619dc2cdf72dfd924e71275506e0
432daiki <shimizudaiki@MacBook-Pro-2.local>
2015-06-27 17:18:00 +0900

    中途半端

---
+++ BESHOP/DetailItemView.h
@@ -0,0 +1,13 @@
+//
+//  DetailItemView.h
+//  BESHOP
+//
+//  Created by 清水 大輝 on 2015/06/27.
+//  Copyright (c) 2015年 Daiki Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailItemView : UIView
+
+@end
---
+++ BESHOP/DrawerViewController.m
@@ -0,0 +1,32 @@
+//
+//  DrawerViewController.m
+//  BESHOP
+//
+//  Created by 清水 大輝 on 2015/06/27.
+//  Copyright (c) 2015年 Daiki Shimizu. All rights reserved.
+//
+
+#import "DrawerViewController.h"
+
+@interface DrawerViewController ()
+
+@end
+
+@implementation DrawerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
