2285db2f819068b3a013b2de4f993f026082de6b
YukiUchiyama <cinnamon409g@gmail.com>
2015-06-28 03:32:14 +0900

    MyItemListのテーブルビュー追加

---
+++ BESHOP/MyItemTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MyItemTableViewCell.m
+//  
+//
+//  Created by Yuki Uchiyama on 2015/06/28.
+//
+//
+
+#import "MyItemTableViewCell.h"
+
+@implementation MyItemTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
