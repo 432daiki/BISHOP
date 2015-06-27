349771bc3cfc4dfee9bfa0b19939bdd7e88a560d
432daiki <shimizudaiki@MacBook-Pro-2.local>
2015-06-27 23:22:40 +0900

    アイテム詳細ビュー完成

--- BESHOP/DetailItemView.m
+++ BESHOP/DetailItemView.m
@@ -10,12 +10,7 @@
 
 @implementation DetailItemView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (IBAction)pushBorrowButton:(id)sender {
 }
-*/
 
 @end
