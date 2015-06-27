9b5326dfdcd38844e77e25f1a21b55d371c9d987
YukiUchiyama <cinnamon409g@gmail.com>
2015-06-27 22:30:11 +0900

    add: initializeview

---
+++ BESHOP/PostViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostViewController.h
+//  BESHOP
+//
+//  Created by Yuki Uchiyama on 2015/06/27.
+//  Copyright (c) 2015年 Daiki Shimizu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostViewController : UIViewController
+
+@end
